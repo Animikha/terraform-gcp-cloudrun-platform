d5787cbd54197e1409271a653996c31a9c1c260a
Animikha <animikhasinha@gmail.com>
2026-01-21 17:46:27 +0000

    Added immutable tags = false

--- terraform/modules/artifact_registry/main.tf
+++ terraform/modules/artifact_registry/main.tf
@@ -2,4 +2,8 @@ resource "google_artifact_registry_repository" "service_repos" {
     for_each = var.repos
     repository_id = "{each.value.name}-service"
     format = "DOCKER"
+
+    docker_config {
+      immutable_tags = false
+    }
 }
