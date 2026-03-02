f130369e45946aa1b47490fbc684cafd690a2f30
Animikha <animikhasinha@gmail.com>
2026-03-02 12:11:57 +0000

    Debugged: resolved app.route issues in all the services

--- services/auth/app.py
+++ services/auth/app.py
@@ -1,20 +1,18 @@
-from flask import Flask, jsonify, request 
+from flask import Flask, jsonify, request
 import os
-
+ 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
+ 
+@app.route("/auth")
+@app.route("/auth/")
+@app.route("/auth/<path:subpath>")
+def api(subpath=None):
     return jsonify({
         "service": os.getenv("SERVICE_NAME", "auth"),
         "environment": os.getenv("ENV", "dev"),
         "version": os.getenv("VERSION", "v1"),
         "path": request.path
     })
-
-@app.route("/health")
-def health():
-    return "OK", 200
-
+ 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
